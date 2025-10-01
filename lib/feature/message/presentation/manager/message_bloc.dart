b890ddef2619abe9b77bc321aabf8374b6f2beff
rezvanmj <rezvan.mjj@gmail.com>
2025-10-01 11:55:37 +0330

    feat: Implement message showing

--- lib/feature/message/presentation/manager/message_bloc.dart
+++ lib/feature/message/presentation/manager/message_bloc.dart
@@ -1,5 +1,3 @@
-import 'dart:developer';
-
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 import 'message_event.dart';
@@ -8,8 +6,6 @@ import 'message_state.dart';
 class MessageBloc extends Bloc<MessageEvent, MessageState> {
   MessageBloc() : super(MessageState()) {
     on<SaveMessage>((event, emit) {
-      log('emittttt');
-      log("New state: ${state.title}, ${state.message}");
       emit(state.copyWith(newMessage: event.message, newTitle: event.title));
     });
   }
